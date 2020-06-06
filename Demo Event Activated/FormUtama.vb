4ae41d498f6f5f04a6a498a383b5989dfe8a7524
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-06-06 20:16:13 +0700

    Add project files.

---
+++ Demo Event Activated/FormUtama.vb
@@ -0,0 +1,11 @@
+﻿Public Class FormUtama
+
+    Private Sub FormUtama_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If MessageBox.Show("Anda yakin?", "Konfirmasi", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            e.Cancel = False 'Lanjutkan menutup form
+        Else
+            e.Cancel = True 'Tidak menutup form
+        End If
+    End Sub
+
+End Class
---
+++ Demo Event Activated/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Demo Event Activated")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Demo Event Activated")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("027630ca-3014-412e-a6ff-19da26036b62")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
